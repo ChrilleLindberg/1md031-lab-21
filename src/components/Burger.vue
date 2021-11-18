64ce29732d7c6fc57037cd284e0a0f1fa05aaf00
ChrilleLindberg <christoffer.lindberg.1806@student.uu.se>
2021-11-18 19:39:40 +0100

    Test

--- src/components/Burger.vue
+++ src/components/Burger.vue
@@ -1,7 +1,14 @@
 <template>
-  <div>
-    {{ burger.name }} {{ burger.kCal }}
-  </div>
+  <section class="burgare">
+
+  <h3>{{burger.name}} {{ burger.kCal }}</h3>
+  <img v-bind:src="burger.img" alt="Burgare" style="width: 400px">
+  <h4>Inehåll:</h4>
+  <ul>
+    <li>{{burger.gluten}}</li>
+    <li>{{burger.lactose}}</li>
+  </ul>
+  </section>
 </template>
 
 <script>
@@ -15,5 +22,26 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.burgare {
 
+  color: #ffaf6e;
+  background-color:#20124d;
+
+  padding: 10px 10px 500px 20px;
+
+}
+.wrapper {
+  display: grid;
+  height: 100px;
+  grid-template-columns: 33% 33% 33%;
+}
+.a {
+  grid-column: 1 ;
+}
+.b {
+  grid-column: 2;
+}
+.c {
+  grid-column: 3;
+}
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,117 @@
 <template>
   <div>
     Burgers
+    <div class="wrapper">
     <Burger v-for="burger in burgers"
             v-bind:burger="burger" 
             v-bind:key="burger.name"/>
   </div>
-  <div id="map" v-on:click="addOrder">
-    click here
   </div>
+  <div id="map" v-on:click="addOrder">
+    click here.
+  </div>
+
+  <header>
+    <div class="head">
+      <img id="pic" src="https://www.damhert.be/content/recipes/271/BURGERWebsite_Hoofding.jpg?v=1" alt="header" title="Header">
+      <div class="center"><h1>Välkommen till MAXade burgare</h1></div>
+    </div>
+  </header>
+  <main>
+    <section class="burgare">
+      <h2>Välj Burgare</h2>
+      <p>Här väljer du burgare</p>
+      <div class="wrapper">
+        <div class="a">
+          <h3>Originalburgare</h3>
+          <img src="https://www.max.se/contentassets/7283b5a0bf324ba99d7fa7e0caf5ae7f/product_max-original.png" alt="Burgare" title="Originalburgare" style="width: 400px">
+          <h4>Inehåll:</h4>
+          <ul>
+            <li>Stora mängder <span id="ingrediens">fett </span></li>
+            <li>Sallad</li>
+            <li>Lök</li>
+          </ul>
+        </div>
+
+        <div class="b">
+          <h3>Friscoburgare</h3>
+          <img src="https://www.max.se/contentassets/093fc9201de04f648ea20afee581bafe/product_frisco-cheese-n-bacon.png" alt="Burgare" title="Friscoburgare" style="width: 400px;">
+          <h4>Inehåll:</h4>
+          <ul>
+            <li>Bröd</li>
+            <li>Sallad</li>
+            <li>Massvis med<span id="ingrediens"> Lök </span></li>
+          </ul>
+        </div>
+        <div class="c">
+          <h3>Dubbel Friscoburgare</h3>
+          <img src="https://www.max.se/contentassets/80851e9c275642b3834b216a880941ff/product_dubbel-friscoburgare.png" alt="Burgare" title="Dubbel Friscoburgare" style="width: 400px;">
+          <h4>Inehåll:</h4>
+          <ul>
+            <li>Dubbel <span id="ingrediens">Bröd </span></li>
+            <li>Dubbel Sallad</li>
+            <li>Dubbel Bacon</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+
+    <section class="order">
+      <h2>Kundinformation</h2>
+      <p>Här anger du nödvändig information</p>
+      <h3>Leveransinformation</h3>
+      <p>
+        <label for="name">Fullständigt namn</label><br>
+        <input type="text" id="name" name="n" required="required" placeholder="För- och efter namn">
+      </p>
+      <p>
+        <label for="email">Email</label><br>
+        <input type="text" id="email" name="e" placeholder="Epostadress">
+      </p>
+      <p>
+        <label for="adress">Adress</label><br>
+        <input type="text" id="adress" name="g" placeholder="Gatunamn">
+      </p>
+      <p>
+        <label for="adressnr"></label><br>
+        <input type="number" id="adressnr" name="gnr" placeholder="Husnummer">
+      </p>
+      <p>
+        <label for="betalning">Betalningsmetod</label>
+        <select id="betalning" name="bet">
+          <option>Swish</option>
+          <option>Natura</option>
+          <option>Kort</option>
+          <option>Faktura</option>
+          <option>Kontant</option>
+        </select>
+      </p>
+      <h4>Kön</h4>
+      <div>
+        <input type="radio" id="man" name="kön" value="man">
+        <label for="huey">Man</label>
+      </div>
+      <div>
+        <input type="radio" id="kvinna" name="kön" value="kvinna">
+        <label for="dewey">Kvinna</label>
+      </div>
+      <div>
+        <input type="radio" id="ib" name="kön" value="ickebinär"
+               checked>
+        <label for="louie">Ickebinär</label>
+      </div>
+      <div id="button">
+        <button type="submit">
+          <img src="https://wolfcoding.com/wp-content/uploads/2021/06/food-delivery.jpg" title="beställning" style="width: 40px;">
+          Bekräfta beställning
+        </button>
+      </div>
+    </section>
+  </main>
+  <hr>
+  <footer>
+    &copy; MAXade burgare
+  </footer>
 </template>
 
 <script>
@@ -17,7 +121,19 @@ import io from 'socket.io-client'
 const socket = io();
 
 
+ function MenuItem (burgerName, calories, yourURL, gluteboolean, laktosboolean) {
+   this.name=burgerName;
+   this.kCal=calories;
+   this.img=yourURL;
+   this.gluten=gluteboolean;
+   this.lactose=laktosboolean;
 
+}
+const Burger1 = new MenuItem("Orginalburgare", 295, "https://www.max.se/contentassets/7283b5a0bf324ba99d7fa7e0caf5ae7f/product_max-original.png", "false", "false");
+const Burger2 = new MenuItem("Friscoburgare", 605, "https://www.max.se/contentassets/093fc9201de04f648ea20afee581bafe/product_frisco-cheese-n-bacon.png", "true", "false");
+const Burger3 = new MenuItem("Dubbel Friscoburgare", 872, "https://www.max.se/contentassets/80851e9c275642b3834b216a880941ff/product_dubbel-friscoburgare.png", "true", "true");
+const hamburgers= [ Burger1, Burger2, Burger3 ]
+console.log(hamburgers)
 
 
 
@@ -29,10 +145,7 @@ export default {
   },
   data: function () {
     return {
-      burgers: [ {name: "small burger", kCal: 250},
-                 {name: "standard burger", kCal: 450},
-                 {name: "large burger", kCal: 850}
-               ]
+      burgers: hamburgers
     }
   },
   methods: {
@@ -58,5 +171,70 @@ export default {
     width: 300px;
     height: 300px;
     background-color: red;
+  }
+  body {
+
+    margin: 50px 50px 50px 50px;
+    font-family: helvetica neue;
+  }
+  .head {
+    overflow:hidden;
+    margin: 50px 0px -60px 0px;
+    padding: 10px 10px 60px 0px;
+
+  }
+  #pic{
+    overflow:hidden;
+    opacity: 60%;
+    width: 1269px;
+    height: 250px;
+  }
+  .center {
+    position: absolute;
+    padding: 30px 90px 30px 90px;
+    text-align: center;
+    font-size: 36px;
+    margin-top: -250px;
+  }
+  .burgare {
+
+    color: #ffaf6e;
+    background-color:#20124d;
+
+    padding: 10px 10px 500px 20px;
+ 
+  }
+  .wrapper {
+    display: grid;
+    height: 100px;
+    grid-template-columns: auto auto auto;
+    border: 3px dashed #eaf90a;
+  }
+  .a {
+    grid-column: 1 ;
+  }
+  .b {
+    grid-column: 2;
+  }
+  .c {
+    grid-column: 3;
+  }
+  #ingrediens {
+    font-family: helvetica neue;
+    text-transform: uppercase;
+  }
+  .order {
+    color: #20124d;
+    background-color:#f2776e;
+    margin: 10px 0px 0px 0px;
+    padding: 10px 10px 0px 20px;
+    border: 3px dashed #eaf90a;
+  }
+  #button{
+    margin: 10px 20px 10px 10px;
+  }
+  button:hover {
+    cursor: pointer;
+    background-color:#eaf90a;
   }
 </style>
